6d0206fc2c331d32c6bea91eb3d13ffe95c2bedc
Eblaraa <baraax01@gmail.com>
2024-05-15 05:02:31 +0700

    unsharp algorithm

--- app/src/main/java/com/example/myapplication/FunctionalitySelectionActivity.kt
+++ app/src/main/java/com/example/myapplication/FunctionalitySelectionActivity.kt
@@ -1,53 +1,64 @@
 package com.example.myapplication
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-
 class FunctionalitySelectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.choose_action)
 
         val photoPath = intent.getStringExtra("photo_path")
 
-        setContentView(R.layout.choose_action)
-
-        // Find the rotation button
+        // Find the buttons
         val rotationButton = findViewById<View>(R.id.Image_rotation)
         val filtersButton = findViewById<View>(R.id.colour_filter)
         val scalingButton = findViewById<View>(R.id.Image_scaling)
         val retouchingButton = findViewById<View>(R.id.Affinetransformations)
+<<<<<<< Updated upstream
         val vectorButton = findViewById<View>(R.id.Vector_editor)
 
-        rotationButton.setOnClickListener {
+=======
+        val unsharpButton = findViewById<View>(R.id.Unsharp_masking)
 
+        // Set click listeners for the buttons
+>>>>>>> Stashed changes
+        rotationButton.setOnClickListener {
             val intent = Intent(this, RotateActivity::class.java)
-            intent.putExtra("photo_path", photoPath) // Pass the photo path to RotateActivity
+            intent.putExtra("photo_path", photoPath)
             startActivity(intent)
         }
+
         filtersButton.setOnClickListener {
             val intent = Intent(this, ColorFiltersActivity::class.java)
             intent.putExtra("photo_path", photoPath)
             startActivity(intent)
         }
+
         scalingButton.setOnClickListener {
             val intent = Intent(this, ImageScalingActivity::class.java)
             intent.putExtra("photo_path", photoPath)
             startActivity(intent)
         }
+
         retouchingButton.setOnClickListener {
             val intent = Intent(this, AffineTrans::class.java)
             intent.putExtra("photo_path" , photoPath)
             startActivity(intent)
         }
 
+<<<<<<< Updated upstream
         vectorButton.setOnClickListener {
             val intent = Intent(this, VectorEditorActivity::class.java)
             intent.putExtra("photo_path" , photoPath)
+=======
+        unsharpButton.setOnClickListener {
+            val intent = Intent(this, UnsharpActivity::class.java)
+            intent.putExtra("photo_path", photoPath)
+>>>>>>> Stashed changes
             startActivity(intent)
         }
     }
