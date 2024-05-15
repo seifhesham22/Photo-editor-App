1216609e0944b80a79ca3815929a63c14df36a63
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2024-05-15 20:02:36 +0700

    Fixing

--- app/src/main/java/com/example/myapplication/FunctionalitySelectionActivity.kt
+++ app/src/main/java/com/example/myapplication/FunctionalitySelectionActivity.kt
@@ -18,14 +18,11 @@ class FunctionalitySelectionActivity : AppCompatActivity() {
         val filtersButton = findViewById<View>(R.id.colour_filter)
         val scalingButton = findViewById<View>(R.id.Image_scaling)
         val retouchingButton = findViewById<View>(R.id.Affinetransformations)
-<<<<<<< Updated upstream
         val vectorButton = findViewById<View>(R.id.Vector_editor)
 
-=======
         val unsharpButton = findViewById<View>(R.id.Unsharp_masking)
 
         // Set click listeners for the buttons
->>>>>>> Stashed changes
         rotationButton.setOnClickListener {
             val intent = Intent(this, RotateActivity::class.java)
             intent.putExtra("photo_path", photoPath)
@@ -50,15 +47,14 @@ class FunctionalitySelectionActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-<<<<<<< Updated upstream
         vectorButton.setOnClickListener {
             val intent = Intent(this, VectorEditorActivity::class.java)
-            intent.putExtra("photo_path" , photoPath)
-=======
+            intent.putExtra("photo_path", photoPath)
+            startActivity(intent)
+        }
         unsharpButton.setOnClickListener {
             val intent = Intent(this, UnsharpActivity::class.java)
             intent.putExtra("photo_path", photoPath)
->>>>>>> Stashed changes
             startActivity(intent)
         }
     }
